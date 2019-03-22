83b2d127ad3b1d5b78ab03ad83cfaf1febbfdb31
Orion <orion.hall@ert.com>
2019-03-22 15:00:22 -0400

    Passing a parameter to a function through a component via an arrow function vs. bind()

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = (props) => {
-    // useState allows us to give a functional component a "state"
-    // this is part of React Hooks
-
-    // useState will return an array with the state and a function which can be used to set the state
-    // so use destructuring to get those into two separate variables
-    // useState will NOT merge the state like setState would
-    const [peopleState, setPeopleState] = useState({
+class App extends Component {
+    state = {
         people: [
             { name: 'Orion Hall', age: 28 },
             { name: 'Jaron Popko', age: 29 },
             { name: 'Molly Ritter', age: 30 }
         ]
-    });
+    };
 
-    const [otherState, setOtherState] = useState('Hi I\'m otherState');
-    console.log(otherState);
-
-    const switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         // On clicking the button call the function provided by useState which will update the state
-        setPeopleState({
+        this.setState({
             people: [
-                { name: 'Britney Spears', age: 28 },
+                { name: newName, age: 28 },
                 { name: 'Madonna Ciccone', age: 29 },
                 { name: 'Robyn Fenty', age: 35 }
             ]
         });
     };
 
-    return (
-        <div className="App">
-            <h1>Hi, I'm a your mom</h1>
-            <p>Yes</p>
-            <button onClick={switchNameHandler}>Switch Name</button>
-            <Person name={peopleState.people[0].name} age={peopleState.people[0].age}/>
-            <Person name={peopleState.people[1].name} age={peopleState.people[1].age}/>
-            <Person name={peopleState.people[2].name} age={peopleState.people[2].age}/>
-        </div>
-    );
-};
-export default app;
+    render () {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a your mom</h1>
+                <p>Yes</p>
+                <button
+                    // Use an arrow function to pass switchNameHandler with a parameter
+                    // This can cause performance implications because a new function is made each time the component renders
+                    onClick={() => this.switchNameHandler('Maximilian')}>Switch Name
+                </button>
+                <Person
+                    name={this.state.people[0].name}
+                    age={this.state.people[0].age}/>
+                <Person
+                    name={this.state.people[1].name}
+                    age={this.state.people[1].age}
+                    // Pass in arguments by using .bind because you are passing the function itself, not running it
+                    click={this.switchNameHandler.bind(this, 'Artemis')}/>
+                <Person
+                    name={this.state.people[2].name}
+                    age={this.state.people[2].age}/>
+            </div>
+        );
+    }
+}
+
+export default App;
