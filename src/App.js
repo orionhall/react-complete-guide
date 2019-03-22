47d915d7ea483b138742f69d85773746f94e2fe0
Orion <orion.hall@ert.com>
2019-03-22 12:40:52 -0400

    React Hooks / useState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = (props) => {
+    // useState allows us to give a functional component a "state"
+    // this is part of React Hooks
+
+    // useState will return an array with the state and a function which can be used to set the state
+    // so use destructuring to get those into two separate variables
+    // useState will NOT merge the state like setState would
+    const [peopleState, setPeopleState] = useState({
+        people: [
+            { name: 'Orion Hall', age: 28 },
+            { name: 'Jaron Popko', age: 29 },
+            { name: 'Molly Ritter', age: 30 }
+        ]
+    });
+
+    const [otherState, setOtherState] = useState('Hi I\'m otherState');
+    console.log(otherState);
+
+    const switchNameHandler = () => {
+        // On clicking the button call the function provided by useState which will update the state
+        setPeopleState({
+            people: [
+                { name: 'Britney Spears', age: 28 },
+                { name: 'Madonna Ciccone', age: 29 },
+                { name: 'Robyn Fenty', age: 35 }
+            ]
+        });
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+            <h1>Hi, I'm a your mom</h1>
+            <p>Yes</p>
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person name={peopleState.people[0].name} age={peopleState.people[0].age}/>
+            <Person name={peopleState.people[1].name} age={peopleState.people[1].age}/>
+            <Person name={peopleState.people[2].name} age={peopleState.people[2].age}/>
+        </div>
     );
-  }
-}
-
-export default App;
+};
+export default app;
